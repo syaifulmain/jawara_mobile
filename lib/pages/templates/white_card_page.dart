fc1d6156e242a1b68bbb110b5ed6d9a721c589f9
VozSoldat <muhamaderril7@gmail.com>
2025-10-15 09:30:06 +0700

    feat: create `WhiteCardPage` as a template, e.g. for `pengeluaran/Daftar`

---
+++ lib/pages/templates/white_card_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:jawara_mobile/constants/colors.dart';
+import 'package:jawara_mobile/constants/rem.dart';
+
+class WhiteCardPage extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  const WhiteCardPage({super.key, required this.title, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      color: AppColors.backgroundColor,
+
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Card(
+          color: AppColors.secondaryColor,
+
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                for (var child in children) ...[
+                  child,
+                ],
+              ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
