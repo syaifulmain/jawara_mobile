26d7ac86a75ab30ce58f1d15bce0451dc09ff3a1
Dhnillhaq <yakulhaq@gmail.com>
2025-11-14 17:12:04 +0700

    refactor:  standardization ui for pemasukan feature

---
+++ lib/data/pemasukan_data.dart
@@ -0,0 +1,16 @@
+import '../models/data_pemasukan_model.dart';
+
+final List<DataPemasukanModel> DummyDataPemasukan = [
+  DataPemasukanModel(
+    nama: 'Dana Hibah',
+    jenisPemasukan: 'Dana Bantuan Pemerintah',
+    tanggal: '12 Oktober 2025',
+    nominal: 'Rp. 20.000.000',
+  ),
+  DataPemasukanModel(
+    nama: 'Gotong Royong Perbaikan Jalan',
+    jenisPemasukan: 'Infrastruktur',
+    tanggal: '19 Oktober 2025',
+    nominal: 'Rp. 15.000.000',
+  ),
+];
---
+++ lib/models/data_pemasukan_model.dart
@@ -0,0 +1,13 @@
+class DataPemasukanModel {
+  final String nama;
+  final String jenisPemasukan;
+  final String tanggal;
+  final String nominal;
+
+  DataPemasukanModel({
+    required this.nama,
+    required this.jenisPemasukan,
+    required this.tanggal,
+    required this.nominal,
+  });
+}
